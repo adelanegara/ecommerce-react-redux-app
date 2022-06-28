14c2772bbfb672fe22ad889660e100f5c5275b7b
Adela <adelangr822@gmail.com>
2022-06-28 21:41:06 +0800

    sort added

--- src/Components/ShopLandingPage/index.js
+++ src/Components/ShopLandingPage/index.js
@@ -1,22 +1,36 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import orderBy from "lodash/orderBy";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { config } from "../../server/config";
 
 const ShopLandingPage = ({ userCart, addChart }) => {
   const [data, setData] = useState();
-  const [filterData, setFilterData] = useState();
   const fetchData = async () => {
     const productList = await axios.get(config.url_products);
     setData(productList.data);
-    setFilterData(productList.data);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const handleSort = (type) => {
+    const sortData = orderBy(data, ["title"], [type]);
+    setData(sortData);
+  };
 
+  const handleSortRate = () => {
+    const sortData = orderBy(
+      data,
+      (item) => {
+        return [item.rating.rate];
+      },
+      ["desc"]
+    );
+    setData(sortData);
+  };
 
   return (
     <>
@@ -26,9 +40,24 @@ const ShopLandingPage = ({ userCart, addChart }) => {
             <div className="card-body">
               <h5 className="card-title">Sort</h5>
               <ul className="list-group">
-                <li className="list-group-item "> Ascending</li>
-                <li className="list-group-item">Descending</li>
-                <li className="list-group-item">Rates</li>
+                <li
+                  className="list-group-item"
+                  onClick={() => handleSort("asc")}
+                >
+                  Ascending
+                </li>
+                <li
+                  className="list-group-item"
+                  onClick={() => handleSort("desc")}
+                >
+                  Descending
+                </li>
+                <li
+                  className="list-group-item"
+                  onClick={() => handleSortRate()}
+                >
+                  Rates
+                </li>
               </ul>
             </div>
           </div>
@@ -50,39 +79,40 @@ const ShopLandingPage = ({ userCart, addChart }) => {
         <div className="col-9 bg-info">
           <div className="container">
             <div className="row">
-              {filterData?.map((item, index) => {
+              {data?.map((item, index) => {
                 return (
-                  <>
-                    <div className="col-4 mt-4">
-                      <div className="ui link cards">
-                        <div className="card" key={index}>
-                          <div className="image">
-                            <img src={item.image} alt={item.id} />
+                  <div className="col-4 mt-4" key={index}>
+                    <div className="ui link cards">
+                      <div className="card">
+                        <div className="image">
+                          <img src={item.image} alt={item.id} />
+                        </div>
+                        <div className="content">
+                          <div className="header">{item.title}</div>
+                          <div className="meta price">$ {item.price}</div>
+                          <div className="meta">
+                            <span> Category: {item.category} </span>
+                            <span> Rate: {item.rating.rate}</span>
                           </div>
-                          <div className="content">
-                            <div className="header">{item.title}</div>
-                            <div className="meta price">$ {item.price}</div>
-                            <div className="meta"><span> Category: {item.category} </span></div>
-                            <div className="form-group d-flex align-items-center">
-                              <button
-                                type="button"
-                                className="btn btn-sm btn-dark button-shop"
-                                onClick={() => addChart(item)}
-                              >
-                                Add Item
-                              </button>
-                              <button
-                                type="button"
-                                className="btn btn-sm btn-dark button-shop"
-                              >
-                                View
-                              </button>
-                            </div>
+                          <div className="form-group d-flex align-items-center">
+                            <button
+                              type="button"
+                              className="btn btn-sm btn-dark button-shop"
+                              onClick={() => addChart(item)}
+                            >
+                              Add Item
+                            </button>
+                            <Link
+                            to={`/view/${item.id}`}
+                            className="btn btn-sm btn-dark mr-1"
+                          >
+                            View 
+                          </Link>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </>
+                  </div>
                 );
               })}
             </div>
--- src/Components/ViewProducts/index.js
+++ src/Components/ViewProducts/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+
 const ViewProduct = () => {
   return (
-    <div>index</div>
+    <div classNameName='container'><div className="row">
+    <div className="col-4 bg-primary">
+      <div classNameName='container'>
+      <div className="card">
+  <div className="card-body">
+  <img className="card-img-top" />
+  </div>
+</div>
+      </div>
+    </div>
+    <div className="col-8 bg-success">co</div>
+  </div></div>
   )
 }
 

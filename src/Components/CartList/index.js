9b9997de5feb7b9dc183ac35b0e4f9a869cd54be
Adela <adelangr822@gmail.com>
2022-06-29 11:03:00 +0800

    table cart added

--- src/Components/CartList/index.js
+++ src/Components/CartList/index.js
@@ -1,11 +1,49 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const CartList = () => {
+const CartList = ({ userCart }) => {
   return (
-    <div>
-      <h1> HAhahahhah</h1>
+    <div className="container">
+      <div className="pt-5">
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Name</th>
+              <th scope="col">Unit Price</th>
+              <th scope="col">QTY</th>
+              <th scope="col">Sub Total</th>
+            </tr>
+          </thead>
+          {userCart?.map((item, index) => {
+            return (
+              <>
+                <tbody>
+                  <tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td> {item.title}</td>
+                    <td>$ {item.price} </td>
+                    <td> QTY</td>
+                    <td> Sub Total</td>
+                  </tr>
+                </tbody>
+              </>
+            );
+          })}
+        </table>
+      </div>
     </div>
   );
 };
 
-export default CartList;
+const mapStateToProps = (state) => ({
+  userCart: state.userCart,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addChart: (payload) => {
+    dispatch({ type: "ADD_CHART", payload });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartList);
